c55d62a3c5873d3c9ab478ca6d309d508b7e55e6
lothand <lothand73@gmail.com>
2025-12-15 14:08:59 +0700

    feat: update footer

--- resources/js/components/footer.tsx
+++ resources/js/components/footer.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import { home, mitra, product } from '@/routes';
+import { Link } from '@inertiajs/react';
+import ContainerWrapper from './fragments/container-wrapper';
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <footer>
+            <ContainerWrapper>
+                <div className="py-10">
+                    <div className="flex flex-col gap-8 md:flex-row md:justify-between">
+                        <div className="max-w-md">
+                            <div className="mb-3 flex items-center gap-2 text-3xl">
+                                ALUS
+                            </div>
+                            <p className="text-sm leading-relaxed">
+                                Lorem ipsum dolor sit amet, consectetur
+                                adipiscing elit. Pellentesque blandit velit, sit
+                                amet euismod risus. Fusce ac eros quis.
+                            </p>
+                        </div>
+                        <div className="flex flex-col gap-3 font-poppins text-3xl">
+                            <Link href={home()} className="hover:opacity-80">
+                                Home
+                            </Link>
 
-export default Footer
+                            <Link href={product()} className="hover:opacity-80">
+                                Produk
+                            </Link>
+
+                            <Link href={mitra()} className="hover:opacity-80">
+                                Mitra
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="mt-10 border-t border-black pt-4 text-xs text-gray-500">
+                        Copyright © 2025 Alus. All Right Reserved
+                    </div>
+                </div>
+            </ContainerWrapper>
+        </footer>
+    );
+};
+
+export default Footer;
