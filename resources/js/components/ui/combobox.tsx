37293ea77c660d616ac73bc2e6bae416ec2a1ff0
Neyr <neyrworks@gmail.com>
2025-12-15 16:48:15 +0700

    feat: remodel login and register page, adding combobox component from shadcn

---
+++ resources/js/components/ui/combobox.tsx
@@ -0,0 +1,82 @@
+"use client"
+
+import * as React from "react"
+import { Check, ChevronsUpDown } from "lucide-react"
+
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+  Command,
+  CommandEmpty,
+  CommandGroup,
+  CommandInput,
+  CommandItem,
+  CommandList,
+} from "@/components/ui/command"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
+
+const frameworks = [
+  {
+    value: "pria",
+    label: "Pria",
+  },
+  {
+    value: "wanite",
+    label: "Wanita",
+  },
+]
+
+export default function Combobox() {
+  const [open, setOpen] = React.useState(false)
+  const [value, setValue] = React.useState("")
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          role="combobox"
+          aria-expanded={open}
+          className="w-full bg-transparent justify-between border-3 border-primary"
+        >
+          {value
+            ? frameworks.find((framework) => framework.value === value)?.label
+            : "Pilih Jenis Kelamin..."}
+          <ChevronsUpDown className="opacity-50" />
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-50 p-0">
+        <Command>
+          <CommandInput placeholder="Search framework..." className="h-9" />
+          <CommandList>
+            <CommandEmpty>Jenis Kelamin tidak ditemukan</CommandEmpty>
+            <CommandGroup>
+              {frameworks.map((framework) => (
+                <CommandItem
+                  key={framework.value}
+                  value={framework.value}
+                  onSelect={(currentValue) => {
+                    setValue(currentValue === value ? "" : currentValue)
+                    setOpen(false)
+                  }}
+                >
+                  {framework.label}
+                  <Check
+                    className={cn(
+                      "ml-auto",
+                      value === framework.value ? "opacity-100" : "opacity-0"
+                    )}
+                  />
+                </CommandItem>
+              ))}
+            </CommandGroup>
+          </CommandList>
+        </Command>
+      </PopoverContent>
+    </Popover>
+  )
+}
--- resources/js/pages/register.tsx
+++ resources/js/pages/register.tsx
@@ -1,9 +1,107 @@
-import React from 'react'
+import MainLayout from '@/components/fragments/main-layout';
+import { Button } from '@/components/ui/button';
+import Combobox from '@/components/ui/combobox';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { login } from '@/routes';
+import { Link } from '@inertiajs/react';
+import { Eye, EyeClosed } from 'lucide-react';
+import { useState } from 'react';
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+    const [showPassword, setShowPassword] = useState(false);
 
-export default Register
+    return (
+        <MainLayout showFooter={false} showNavbar={false}>
+            <div className="flex h-screen w-screen items-center justify-center">
+                <div className="flex h-fit w-fit shadow-md">
+                    <div className="min-h-[65vh] w-md">
+                        <img
+                            src="#"
+                            alt="ini adalah foto navbar"
+                            className="h-full w-full rounded-l-2xl border-2 object-cover"
+                        />
+                    </div>
+                    <form className="flex min-h-[65vh] w-md flex-col items-start justify-center gap-6 rounded-r-2xl bg-white p-10">
+                        <div className="flex flex-col gap-2">
+                            <p className="text-4xl font-bold text-primary">
+                                Daftar
+                            </p>
+                            <p className="text-xs opacity-80">
+                                Silahkan isi data berikut dengan lengkap dan
+                                sesuai
+                            </p>
+                        </div>
+                        <div className="flex w-full flex-col gap-10">
+                            <div className="flex flex-col gap-4">
+                                <div className="flex flex-col gap-2">
+                                    <Label>Nama Lengkap</Label>
+                                    <Input
+                                        type="text"
+                                        placeholder="Nama lengkap"
+                                    />
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <Label>Username</Label>
+                                    <Input type="text" placeholder="Username" />
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <Label>Email</Label>
+                                    <Input
+                                        type="email"
+                                        placeholder="Contoh@gmail.com"
+                                    />
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <Label>Password</Label>
+                                    <div className="relative">
+                                        <Input
+                                            type={
+                                                showPassword
+                                                    ? 'text'
+                                                    : 'password'
+                                            }
+                                            placeholder="Masukan passwordmu di sini"
+                                        />
+                                        <Button
+                                            type="button"
+                                            onClick={() =>
+                                                setShowPassword(!showPassword)
+                                            }
+                                            className="absolute top-1/2 right-1 -translate-y-1/2 bg-transparent text-primary hover:bg-transparent"
+                                        >
+                                            {showPassword ? (
+                                                <EyeClosed />
+                                            ) : (
+                                                <Eye />
+                                            )}
+                                        </Button>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col gap-2">
+                                    <Label>Jenis Kelamin</Label>
+                                    <Combobox />
+                                </div>
+                            </div>
+                            <div className="flex w-full flex-col gap-4">
+                                <Button type='submit' size={'lg'}>Masuk</Button>
+                                <p className="w-full text-center text-xs">
+                                    Sudah punya akun?
+                                    <Link
+                                        href={login()}
+                                        className="font-bold text-primary"
+                                    >
+                                        {' '}
+                                        Masuk
+                                    </Link>
+                                </p>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </MainLayout>
+    );
+};
+
+export default Register;
